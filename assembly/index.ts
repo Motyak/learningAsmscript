8da09f6a86c21ffdaf60521981cc7fdaa529705a
Motyak <m0tyak_@live.fr>
2020-11-15 01:06:35 +0000

    lets try

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,16 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+/** Calculates the n-th Fibonacci number. */
+export function fib(n: u16): u32 {
+  var a: u32 = 0
+  var b: u32 = 1
+  if (n > 0) {
+    while (--n) {
+      let t = a + b
+      a = b
+      b = t
+    }
+    return b
+  }
+  return a
 }
